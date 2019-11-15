ff5b01ae0d4babe32db3fdcc8e94af762103c413
oliviaschneider <57157804+oliviaschneider@users.noreply.github.com>
2019-11-15 17:08:01 -0500

    Update chris.js

--- chris.js
+++ chris.js
@@ -43,28 +43,4 @@ function calcSpeed(prev, next) {
 
 }
 
-<!-- End of greta animation --> 
-     
-        $(document).ready(function(e) {
-        var width = $(document).width();
 
-        function goRight() {
-            $(".b").animate({
-            left: width
-          }, 5000, function() {
-             setTimeout(goLeft, 50);
-          });
-        }
-        function goLeft() {
-            $(".b").animate({
-            left: 0
-          }, 5000, function() {
-             setTimeout(goRight, 50);
-          });
-        }
-
-        setTimeout(goRight, 50);
-    });
-
-     
-   
