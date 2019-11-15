e756c1ca4187bf2d658b402566f8d0e0fc9884bd
oliviaschneider <57157804+oliviaschneider@users.noreply.github.com>
2019-11-15 14:37:04 -0500

    Update chris.js

--- chris.js
+++ chris.js
@@ -49,14 +49,14 @@ function calcSpeed(prev, next) {
         var width = $(document).width();
 
         function goRight() {
-            $("#animate").animate({
+            $(".b").animate({
             left: width
           }, 5000, function() {
              setTimeout(goLeft, 50);
           });
         }
         function goLeft() {
-            $("#animate").animate({
+            $(".b").animate({
             left: 0
           }, 5000, function() {
              setTimeout(goRight, 50);
